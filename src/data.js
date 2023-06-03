143fe0e1d28cf557d108e9cb9c55286a6f318fed
mbatorek7 <maegan.batorek@valpo.edu>
2023-06-02 20:01:34 -0500

    adding images folder

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Inquirer and Tests",
     description:
       "With this app, the user can easily create a team profile that can consist of manager(s), engineer(s), and intern(s). The web app created will displayed all necessary information and allow the user to email any employee that it chooses with the press of a button.",
-    image: "./project-1.gif",
+    image: "./images/project-1.gif",
     link: "https://github.com/mbatorek7/hw10-team-profile",
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     subtitle: "Express",
     description:
       "With this application, the user can create a list of notes. Each note is displayed on the side of the screen. When the user clicks on the note, they can see the title and text that was written. The user's notes will be saved unless the user presses the delete button.",
-    image: "./project-2.gif",
+    image: "./images/project-2.gif",
     link: "https://batorek-note-taker.herokuapp.com/",
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     subtitle: "Object-relational mapping",
     description:
       "With this project, the user can get all categories, products, or tags along with related information. The user can also obtain a singular category, product, or tag by ID and they can delete or update by ID as well. The user can also create a new category, product, or tag.",
-    image: "./project-3.gif",
+    image: "./images/project-3.gif",
     link: "https://github.com/mbatorek7/hw13-ORM",
   },
   {
@@ -28,7 +28,7 @@ export const projects = [
     subtitle: "Third-Party APIs",
     description:
       "This application is designed to let you easily manage your busy workday schedule. It allows you to see the exact date and time at the top. With that in mind, you will also be able to see which timeslots have past, which one is the present, and which timeslots are upcoming based on the colors.",
-    image: "./project-4.gif",
+    image: "./images/project-4.gif",
     link: "https://mbatorek7.github.io/hw5-workday-scheduler/",
   },
 ];
