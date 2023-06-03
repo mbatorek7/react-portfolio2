ecf374dba2ba32faedadb648bca517792b33a7fd
mbatorek7 <maegan.batorek@valpo.edu>
2023-06-02 21:34:28 -0500

    added worker

--- src/components/ViewResume.js
+++ src/components/ViewResume.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Document, Page, pdfjs } from 'react-pdf';
 import resumePDF from '../images/updated-resume.pdf';
 
 export default function ViewResume() {
+
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
+
     pdfjs.GlobalWorkerOptions.workerSrc = new URL(
         'pdfjs-dist/build/pdf.worker.min.js',
         import.meta.url,
@@ -10,7 +18,7 @@ export default function ViewResume() {
 
     return (
         <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-            <Document file={resumePDF}>
+            <Document file={resumePDF} onLoadSuccess={onDocumentLoadSuccess}>
                 <Page pageNumber={1} />
             </Document>
         </div>
