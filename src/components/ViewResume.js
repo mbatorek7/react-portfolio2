22195e8e13913fd363e8565bb9f39a0c4dd3ca22
mbatorek7 <maegan.batorek@valpo.edu>
2023-06-02 20:19:07 -0500

    view resume x3

--- src/components/ViewResume.js
+++ src/components/ViewResume.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Document from 'react-pdf';
+import { Document, Page } from 'react-pdf';
+import resumePDF from './updated-resume.pdf';
 
 export default function Resume() {
     return (
         <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-            <Document file="updated-resume.pdf"></Document>
+            <Document file={resumePDF}>
+                <Page pageNumber={1} />
+            </Document>
         </div>
     );
 }
