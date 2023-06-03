5a66f7364b66d4be8e985d5a584f833c00ffb0ed
mbatorek7 <maegan.batorek@valpo.edu>
2023-06-02 21:27:20 -0500

    pdf view

--- src/components/ViewResume.js
+++ src/components/ViewResume.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Document, Page } from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
 import resumePDF from '../images/updated-resume.pdf';
 
 export default function Resume() {
+    pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+        'pdfjs-dist/build/pdf.worker.min.js',
+        import.meta.url,
+    ).toString();
+
     return (
         <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-            <Document file={{
-                data: resumePDF
-            }}>
+            <Document file={resumePDF}>
                 <Page pageNumber={1} />
             </Document>
         </div>
