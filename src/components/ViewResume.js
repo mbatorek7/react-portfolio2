ddedc5c43bfc4f351928aad12a5cc08b5658bcf2
mbatorek7 <maegan.batorek@valpo.edu>
2023-06-02 20:16:45 -0500

    view resume x2

--- src/components/ViewResume.js
+++ src/components/ViewResume.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Document } from 'react-pdf';
+import Document from 'react-pdf';
 
 export default function Resume() {
     return (
         <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-            <Document file="./updated-resume.pdf" onLoadSuccess={onDocumentLoadSuccess}></Document>
+            <Document file="updated-resume.pdf"></Document>
         </div>
     );
 }
