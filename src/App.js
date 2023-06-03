bb34859528c57da528c0c7343e23238688730394
mbatorek7 <maegan.batorek@valpo.edu>
2023-06-02 21:42:46 -0500

    forgot import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { pdfjs } from 'react-pdf';
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
