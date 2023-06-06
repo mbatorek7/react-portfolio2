1270ea662780e5b1b98a147329e4a96b12cdd2bb
mbatorek7 <maegan.batorek@valpo.edu>
2023-06-06 13:55:30 -0500

    updated resume file and added routes

--- src/App.js
+++ src/App.js
@@ -7,21 +7,20 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume";
-import ViewResume from "./components/ViewResume";
 
 export default function App() {
     return (
         <main className="text-gray-400 bg-gray-900 body-font">
             <Navbar />
+            <About />
             <Routes>
                 <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/resume" element={<Resume />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
 
-            <Projects />
-            <Skills />
-            <Resume />
-            <ViewResume />
-            <Contact />
             <Footer />
         </main>
     );
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from 'react-pdf';
+import resumePDF from '../images/updated-resume.pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.js',
+    import.meta.url,
+).toString();
 
 export default function Resume() {
     return (
@@ -6,11 +13,18 @@ export default function Resume() {
             <h3 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                 Download my resume here!
             </h3>
+
             <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                 <a className="button" href="../../public/updated-resume.pdf" download="updated-resume.pdf">
                     Download Resume
                 </a>
             </button>
+
+            <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+                <Document onError={console.error} file={resumePDF}>
+                    <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
+                </Document>
+            </div>
         </div>
     );
 }
