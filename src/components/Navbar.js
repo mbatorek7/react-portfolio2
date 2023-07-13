7461ab61bbc27d07c4627b7a71497a15f14d9991
mbatorek7 <maegan.batorek@valpo.edu>
2023-07-13 12:44:55 -0500

    link issue and new resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ export default function Navbar() {
         <header className="bg-gray-800 md:sticky top-0 z-10">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="title-font font-medium text-white mb-4 md:mb-0">
-                    <a href="/about" className="ml-3 text-xl">
+                    <Link to="/about" className="ml-3 text-xl">
                         Maegan Batorek
-                    </a>
+                    </Link>
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
                     <ul>
